b1cd9a6f141e402e5e5ebe65df9c986903b71c32
mardi2020 <mardi@khu.ac.kr>
2024-10-26 14:51:44 +0900

    feat(week4): 라인 스킵 기능 테스트

--- src/main/java/com/mardi2020/exbatch/config/FlatFileItemJobConfig.java
+++ src/main/java/com/mardi2020/exbatch/config/FlatFileItemJobConfig.java
@@ -10,9 +10,11 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.skip.SkipPolicy;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
@@ -75,6 +77,8 @@ public class FlatFileItemJobConfig {
                 .<Customer, Customer>chunk(CHUNK_SIZE, transactionManager)
                 .reader(flatFileItemReader())
                 .processor(itemProcessor) //
+                .faultTolerant()
+                .skipPolicy((exception, skipCount) -> exception instanceof FlatFileParseException) // skip 정책은 faultTolerant() 호출 후 수행
                 .writer(flatFileItemWriter())
                 .build();
     }
